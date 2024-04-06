00312cb524a289174574e711e2e2f99a00975269
salmabenhassin <salmabenhssin52@gmail.com>
2024-04-06 17:58:19 +0100

    first

---
+++ app_gestion_note/src/main/java/com/example/demo/AppGestionNoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGestionNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppGestionNoteApplication.class, args);
+	}
+
+}
